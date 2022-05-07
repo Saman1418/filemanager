6aea530257a1c18df753f1c8f49675843fb67fb6
saman <samanvay.agarwal07@gmail.com>
2022-05-08 01:09:38 +0530

    login

--- routes/authanticationApi/Login/login.js
+++ routes/authanticationApi/Login/login.js
@@ -9,35 +9,42 @@ const firebase = require("../../../firebase/FirebaseConfig");
 const firestore = firebase.firestore();
 const date = firebase.firestore.FieldValue.serverTimestamp();
 
-
-
 router.post("/Login", async (req, res) => {
   let collectionRef = firestore.collection("users");
   const LoginData = {
-      email:req.body.email,
-      password:req.body.password,
-  }
+    email: req.body.email,
+    password: req.body.password,
+  };
   try {
     const loginUser = await firebase
       .auth()
-      .signInWithEmailAndPassword(LoginData.email,LoginData.password);
+      .signInWithEmailAndPassword(LoginData.email, LoginData.password);
 
-      const resp = {
-        uid:loginUser.user.uid,
-        name:loginUser.user.displayName,
-        email:loginUser.user.email
-      }
+    const resp = {
+      uid: loginUser.user.uid,
+      name: loginUser.user.displayName,
+      email: loginUser.user.email,
+    };
 
-    
-    const userData = await collectionRef.where("uid","==",loginUser.user.uid).get();
-    // console.log("docs", userData.docs);
-    res.status(200).send(resp);
+    // const userData = await collectionRef.where("uid","==",loginUser.user.uid).get();
+    // console.log("docs", userData.data)
+    // const aa = userData.data()
+
+    const unsub = collectionRef
+      .where("uid", "==", loginUser.user.uid)
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach((doc) => {
+          console.log(doc.data());
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        res.status(200).send({ documents });
+      });
+    // res.status(200).send({ data: resp, aa });
   } catch (e) {
     console.log(e);
     res.status(400).send({ message: "Please check your login credentials" });
   }
 });
-
-
 
 module.exports = router;
