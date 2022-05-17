9a0900ddb974c4bedf74ce82b56cf5ad55852735
saman <samanvay.agarwal07@gmail.com>
2022-05-17 11:29:41 +0530

    login

--- firebase/FirebaseConfig.js
+++ firebase/FirebaseConfig.js
@@ -2,13 +2,13 @@
 const firebase = require("firebase")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzBv0o89Ytoje6JAMokFvgBP_oxGfLtEo",
-  authDomain: "artistic-yoga-library.firebaseapp.com",
-  projectId: "artistic-yoga-library",
-  storageBucket: "artistic-yoga-library.appspot.com",
-  messagingSenderId: "353148853074",
-  appId: "1:353148853074:web:47d0bc21fa3127f399f88b",
-  measurementId: "G-Y2T7QBQ8DW"
+  apiKey: "AIzaSyCPdRwqMogW4ykOaRlBVVDu46kMQOlIu34",
+  authDomain: "orgfilemanager.firebaseapp.com",
+  projectId: "orgfilemanager",
+  storageBucket: "orgfilemanager.appspot.com",
+  messagingSenderId: "1000255133338",
+  appId: "1:1000255133338:web:e65063986c4ce0793a3123",
+  measurementId: "G-8H4RM9HWGL"
 };
 
 // if (!firebase.apps.length) {
